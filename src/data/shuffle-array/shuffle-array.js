f39bb9533fd2de247950fcdba4355817bb699805
Philipp Neumann <p.neumann@supernice-dev.com>
2020-07-29 09:37:46 +0200

    add shuffle array

---
+++ src/data/shuffle-array/shuffle-array.js
@@ -0,0 +1,12 @@
+export function shuffleArray (array)
+{
+  for (let idx = array.length - 1; idx > 0; idx--)
+  {
+    const rndm = Math.floor(Math.random() * (idx + 1))
+    const item = array[idx]
+    array[idx] = array[rndm]
+    array[rndm] = item
+  }
+
+  return array
+}
