5e927aa90a63be154915aebd1a0dcfc717ab09f6
Philipp Neumann <p.neumann@supernice-dev.com>
2020-09-02 21:14:41 +0200

    remove /* eslint-disable-next-line */

--- src/data/localStrorage.js
+++ src/data/localStrorage.js
@@ -1,6 +1,5 @@
 export function getLocalStorageItem (pId)
 {
-  /* eslint-disable-next-line */
   const ls = localStorage || window.localStorage
   if (ls) return JSON.parse(ls.getItem(pId))
 }
@@ -8,7 +7,6 @@ export function getLocalStorageItem (pId)
 
 export function setLocalStorageItem (pId, pValue)
 {
-  /* eslint-disable-next-line */
   const ls = localStorage || window.localStorage
   if (ls) ls.setItem(pId, JSON.stringify(pValue))
 }
