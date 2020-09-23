6f6ced9df0d2c099427a0f5993ecb9faefb49f1c
Philipp Neumann <p.neumann@supernice-dev.com>
2020-09-23 23:04:51 +0200

    export shuffleArray

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export { default as isUserAgentSearchBot } from './browser/isUserAgentSearchBot'
 // import * as browser from './browser'
 // export {browser}
 
+export { default as shuffleArray } from './data/shuffleArray'
 export { default as cleanPath } from './string/cleanPath'
 export { default as fixedEncodeURIComponent } from './string/fixedEncodeURIComponent'
 export { default as replaceUmlauts } from './string/replaceUmlauts'
