f94949862c9b6687ef0d960297dc37493900d3db
Philipp Neumann <p.neumann@supernice-dev.com>
2021-08-20 12:45:36 +0200

    update loadScript

--- src/script/loadScript.js
+++ src/script/loadScript.js
@@ -1,15 +1,17 @@
 // https://stackoverflow.com/questions/41728807/how-to-embed-a-script-in-jsx-in-a-component
 
 
-export default function loadScript (pUrl, pCallback)
+export default function loadScript (url, onLoad)
 {
   const script = document.createElement('script')
   script.type = 'text/javascript'
   script.onload = (evt) =>
   {
     // console.log('evt: ', evt)
-    if (pCallback) pCallback()
+    // document.body.removedChild(script)
+    if (onLoad) onLoad()
   }
-  script.src = pUrl
+  
+  script.src = url
   document.body.appendChild(script)
 }
