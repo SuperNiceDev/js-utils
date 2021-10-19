31a7f527602723b0757cc5d11033094d037a77d3
Philipp Neumann <p.neumann@supernice-dev.com>
2021-10-19 12:09:46 +0200

    improve insertAndShiftArray

--- src/data/insertAndShiftArray.js
+++ src/data/insertAndShiftArray.js
@@ -1,5 +1,5 @@
 const insertAndShiftArray = (arr, from, to) => {
-  const newArr = arr.map((item) => item);
+  const newArr = [...arr];
   const cutOut = newArr.splice(from, 1)[0]; // cut the element at index 'from'
   newArr.splice(to, 0, cutOut); // insert it at index 'to'
 
